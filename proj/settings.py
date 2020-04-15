726b6e871e1079cbde7e216fe7e75d39868fbe0a
harshith <svs.harshith@gmail.com>
2020-04-15 23:53:39 +0530

    env added

--- proj/settings.py
+++ proj/settings.py
@@ -9,13 +9,12 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+from decouple import config
 EMAIL_USE_TLS= True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='opzomp@gmail.com'
-EMAIL_HOST_PASSWORD='films@opzo'
+EMAIL_HOST_USER=config('email')
+EMAIL_HOST_PASSWORD=config('password')
 EMAIL_PORT= 587
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
