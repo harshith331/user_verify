820ad8f08a93082fb27e9036d74bb01de4f9f45b
harshith <svs.harshith@gmail.com>
2020-04-19 13:46:18 +0530

    admin list

--- verif_usr/admin.py
+++ verif_usr/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 class userAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name','state','fav_gnr_writing', 'ready')
+    list_display = ('email', 'first_name','state','fav_gnr_writing','phone_no', 'ready')
 admin.site.register(user,userAdmin)
